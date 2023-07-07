1a6447f63075dc6c24dd31d313ecbffae391d7d2
Jon Ribeiro <contact@jonathas.com>
2023-07-07 12:03:47 +0200

    chore: encapsulation

--- bank/accounts/checkingAccount.go
+++ bank/accounts/checkingAccount.go
@@ -14,17 +14,17 @@ type CheckingAccount struct {
 	Holder        customers.Holder
 	AgencyNumber  int
 	AccountNumber int
-	Balance       float64
+	balance       float64 // private attribute (encapsulated!)
 }
 
 // c points to who's calling the function
 func (c *CheckingAccount) Withdraw(value float64) string {
 	fmt.Println("Withdrawing", value)
 
-	isWithDrawAllowed := value <= c.Balance && value > 0
+	isWithDrawAllowed := value <= c.balance && value > 0
 
 	if isWithDrawAllowed {
-		c.Balance -= value
+		c.balance -= value
 		return "Withdrawal successful"
 	}
 	return "Insufficient funds"
@@ -34,19 +34,24 @@ func (c *CheckingAccount) Deposit(value float64) (string, float64) {
 	fmt.Println("Depositing", value)
 
 	if value > 0 {
-		c.Balance += value
-		return "Deposit successful", c.Balance
+		c.balance += value
+		return "Deposit successful", c.balance
 	}
-	return "Invalid deposit value", c.Balance
+	return "Invalid deposit value", c.balance
 }
 
 func (c *CheckingAccount) Transfer(value float64, destinationAccount *CheckingAccount) bool {
 	fmt.Println("Transferring", value)
 
-	if value < c.Balance && value > 0 {
-		c.Balance -= value
-		destinationAccount.Balance += value
+	if value < c.balance && value > 0 {
+		c.balance -= value
+		destinationAccount.balance += value
 		return true
 	}
 	return false
+}
+
+// in order to access the balance attribute, we need to create a getter
+func (c *CheckingAccount) GetBalance() float64 {
+	return c.balance
 }
--- bank/main.go
+++ bank/main.go
@@ -4,35 +4,44 @@ import (
 	"fmt"
 
 	"github.com/jonathas/learning-go/bank/accounts"
+	"github.com/jonathas/learning-go/bank/customers"
 )
 
 func structsAndPointers() {
+	jon := customers.Holder{Name:"Jon", Document:"123.123.123-12", Job:"Developer"}
+
 	checkingAccount1 := accounts.CheckingAccount{
-		Holder: "Jon",
+		Holder: jon,
 		AgencyNumber: 589,
-		AccountNumber: 123456,
-		Balance: 125.50}
+		AccountNumber: 123456}
+
+	checkingAccount1.Deposit(125.50)
 
 	fmt.Println(checkingAccount1)
 
-	checkingAccount2 := accounts.CheckingAccount{"Estela",222,111222,200}
+	estela := customers.Holder{Name:"Estela"}
+	checkingAccount2 := accounts.CheckingAccount{Holder:estela,AgencyNumber:222,AccountNumber:111222}
+	checkingAccount2.Deposit(200)
 
 	fmt.Println(checkingAccount2)
 
-	checkingAccount3 := accounts.CheckingAccount{"Estela",222,111222,200}
+	checkingAccount3 := accounts.CheckingAccount{Holder:estela,AgencyNumber:222,AccountNumber:111222}
+	checkingAccount3.Deposit(200)
 
 	// Comparing two structs, returns true
 	fmt.Println(checkingAccount2 == checkingAccount3)
 
 	myAccount1 := new(accounts.CheckingAccount)
-	myAccount1.Holder = "Jon"
-	myAccount1.Balance = 500
+	myAccount1.Holder = jon
+
+	myAccount1.Deposit(500)
 
 	fmt.Println(myAccount1)
 
 	myAccount2 := new(accounts.CheckingAccount)
-	myAccount2.Holder = "Jon"
-	myAccount2.Balance = 500
+	myAccount2.Holder = jon
+
+	myAccount2.Deposit(500)
 
 	// Comparing two pointers, returns false
 	fmt.Println(myAccount1 == myAccount2)
@@ -57,9 +66,13 @@ func sum(numbers ...int) int {
 func main() {
 	structsAndPointers()
 
+	silvia := customers.Holder{Name:"Silvia"}
+	jon := customers.Holder{Name:"Jon"}
+
 	silviaAccount := accounts.CheckingAccount{}
-	silviaAccount.Holder = "Silvia"
-	silviaAccount.Balance = 500
+	silviaAccount.Holder = silvia
+
+	silviaAccount.Deposit(500)
 
 	fmt.Println(silviaAccount)
 
@@ -69,17 +82,18 @@ func main() {
 	fmt.Println(status, value)
 
 	jonAccount := new(accounts.CheckingAccount)
-	jonAccount.Holder = "Jon"
-	jonAccount.Balance = 500
+	jonAccount.Holder = jon
 
-	fmt.Println("Jon's account balance:", jonAccount.Balance)
-	fmt.Println("Silvia's account balance:", silviaAccount.Balance)
+	jonAccount.Deposit(500)
+
+	fmt.Println("Jon's account balance:", jonAccount.GetBalance())
+	fmt.Println("Silvia's account balance:", silviaAccount.GetBalance())
 
 	fmt.Println("Silvia transferring 100 to Jon:")
 	silviaAccount.Transfer(100, jonAccount)
-	fmt.Println("Silvia's account balance after transfer:", silviaAccount.Balance)
+	fmt.Println("Silvia's account balance after transfer:", silviaAccount.GetBalance())
 
-	fmt.Println("Jon's account balance after transfer:", jonAccount.Balance)
+	fmt.Println("Jon's account balance after transfer:", jonAccount.GetBalance())
 
 	fmt.Println(sum(1))
 	fmt.Println(sum(1,1))
